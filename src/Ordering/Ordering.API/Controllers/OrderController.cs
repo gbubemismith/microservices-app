2e70fc3f5642df45c673dd51e8ba4be5e6949f48
gbubemismith <gsmithwalter@gmail.com>
2020-12-16 01:05:05 +0100

    Added mediatr and API controller

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
 
 namespace Ordering.API.Controllers
 {
@@ -6,6 +10,29 @@ namespace Ordering.API.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-        
+        private readonly IMediator _mediatr;
+
+        public OrderController(IMediator mediatr)
+        {
+            _mediatr = mediatr;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetOrdersByUsername(string username)
+        {
+            var query = new GetOrderByUsernameQuery(username);
+
+            var orders = await _mediatr.Send(query);
+
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckoutOrder(CheckoutOrderCommand command)
+        {
+            var result = await _mediatr.Send(command);
+
+            return Ok(result);
+        }
     }
 }
